a990b6210bfd98b86213f48307c2415717843af3
wedodge89 <50926033+wedodge89@users.noreply.github.com>
2020-09-07 17:21:52 -0500

    built react app with GraphQL backend

---
+++ todo/src/App.js
@@ -0,0 +1,141 @@
+import React from "react"
+import { useQuery, useMutation, gql } from "@apollo/client"
+import { Todos } from "react-todomvc"
+
+import "react-todomvc/dist/todomvc.css"
+
+const GET_TODOS = gql`
+  query {
+    queryTodo {
+      id
+      value
+      completed
+    }
+  }
+`
+
+const ADD_TODO = gql`
+  mutation addTodo($todo: AddTodoInput!) {
+    addTodo(input: [$todo]) {
+      todo {
+        id
+        value
+        completed
+      }
+    }
+  }
+`
+
+const UPDATE_TODO = gql`
+  mutation updateTodo($id: ID!, $todo: TodoPatch!) {
+    updateTodo(input: { filter: { id: [$id] }, set: $todo }) {
+      todo {
+        id
+        value
+        completed
+      }
+    }
+  }
+`
+
+const DELETE_TODO = gql`
+  mutation deleteTodo($id: ID!) {
+    deleteTodo(filter: { id: [$id] }) {
+      todo {
+        id
+      }
+    }
+  }
+`
+
+const CLEAR_COMPLETED_TODOS = gql`
+  mutation updateTodo {
+    deleteTodo(filter: { completed: true }) {
+      todo {
+        id
+      }
+    }
+  }
+`
+
+function App() {
+  const [add] = useMutation(ADD_TODO)
+  const [del] = useMutation(DELETE_TODO)
+  const [upd] = useMutation(UPDATE_TODO)
+  const [clear] = useMutation(CLEAR_COMPLETED_TODOS)
+
+  const { loading, error, data } = useQuery(GET_TODOS)
+  if (loading) return <p>Loading</p>
+  if (error) {
+    return <p>`Error: ${error.message}`</p>
+  }
+
+  const addNewTodo = (value) =>
+    add({
+      variables: { todo: { value: value, completed: false } },
+      update(cache, { data }) {
+        const existing = cache.readQuery({ query: GET_TODOS })
+        cache.writeQuery({
+          query: GET_TODOS,
+          data: {
+            queryTodo: [
+              ...(existing ? existing.queryTodo : []),
+              ...data.addTodo.todo,
+            ],
+          },
+        })
+      },
+    })
+
+  const updateTodo = (modifiedTodo) =>
+    upd({
+      variables: {
+        id: modifiedTodo.id,
+        todo: {
+          value: modifiedTodo.value,
+          completed: modifiedTodo.completed,
+        },
+      },
+      update(cache, { data }) {
+        data.updateTodo.todo.map((t) =>
+          cache.modify({
+            id: cache.identify(t),
+            fields: {
+              value: () => t.value,
+              completed: () => t.completed,
+            },
+          })
+        )
+      },
+    })
+
+  const deleteTodo = (id) =>
+    del({
+      variables: { id },
+      update(cache, { data }) {
+        data.deleteTodo.todo.map(t => cache.evict({ id: cache.identify(t) }))
+      },
+    })
+
+  const clearCompletedTodos = () =>
+    clear({
+      update(cache, { data }) {
+        data.deleteTodo.todo.map(t => cache.evict({ id: cache.identify(t) }))
+      },
+    })
+
+  return (
+    <div>
+      <Todos
+        todos={data.queryTodo}
+        addNewTodo={addNewTodo}
+        updateTodo={updateTodo}
+        deleteTodo={deleteTodo}
+        clearCompletedTodos={clearCompletedTodos}
+        todosTitle="GraphQL Todos"
+      />
+    </div>
+  )
+}
+
+export default App
---
+++ todo/src/index.js
@@ -0,0 +1,29 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import App from "./App"
+import {
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache,
+  createHttpLink,
+} from "@apollo/client"
+
+const createApolloClient = () => {
+  const httpLink = createHttpLink({
+    uri: "https://to-do-practice.us-west-2.aws.cloud.dgraph.io/graphql",
+  })
+
+  return new ApolloClient({
+    link: httpLink,
+    cache: new InMemoryCache(),
+  })
+}
+
+ReactDOM.render(
+  <ApolloProvider client={createApolloClient()}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ApolloProvider>,
+  document.getElementById("root")
+)
